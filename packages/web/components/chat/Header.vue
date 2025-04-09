2465c83ae4bc9270b6e039d5c849cbb40862e4a8
haandol <ldg55d@gmail.com>
2025-04-09 09:35:46 +0000

    feat: apply cursor pointer

--- packages/web/components/chat/Header.vue
+++ packages/web/components/chat/Header.vue
@@ -5,10 +5,10 @@
       <span v-if="!isMinimized" class="font-semibold text-gray-800 text-sm">Open Rufus</span>
     </div>
     <div class="flex items-center gap-2">
-      <button @click="emit('minimize')" class="text-gray-400 hover:text-gray-600">
+      <button @click="emit('minimize')" class="text-gray-400 hover:text-gray-600 cursor-pointer">
         <i :class="['pi text-sm', isMinimized ? 'pi-window-maximize' : 'pi-window-minimize']"></i>
       </button>
-      <button @click="emit('close')" class="text-gray-400 hover:text-gray-600">
+      <button @click="emit('close')" class="text-gray-400 hover:text-gray-600 cursor-pointer">
         <i class="pi pi-times text-sm"></i>
       </button>
     </div>
