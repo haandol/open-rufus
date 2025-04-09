c87bfe445512601ac7b7ae3684cdd2e467768878
haandol <ldg55d@gmail.com>
2025-04-09 08:21:09 +0000

    apply dotenv

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,31 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import * as path from 'path'
-
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: false,
   vite: {
     plugins: [tailwindcss()],
   },
-  css: ['@/assets/css/app.css', 'primeicons/primeicons.css'],
+  css: ["@/assets/css/app.css", "primeicons/primeicons.css"],
 
   runtimeConfig: {
     public: {
-      apiUrl: 'http://localhost:8000',
+      apiUrl: process.env.API_URL,
     },
   },
 
   modules: [
-    '@vueuse/nuxt',
+    "@vueuse/nuxt",
     [
-    '@pinia/nuxt',
-    {
-        autoImports: ['defineStore', 'definePiniaStore', 'acceptHMRUpdate'],
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "definePiniaStore", "acceptHMRUpdate"],
       },
     ],
-    '@nuxt/image',
+    "@nuxt/image",
   ],
-})
+});
